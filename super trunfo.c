64bb4b37c4347899f04672f91f17418381c3bd8f
Phelipo21 <ypheliporego@gmail.com>
2025-08-26 14:31:26 +0000

    população

---
+++ super trunfo.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main () { 
+
+    printf(" desafio  super trunfo\n");
+
+
+
+    printf("carta 1\n");
+    char codigo[20];
+    char nome[20];
+    int populaçao;
+    float area;
+    float pib;
+    int pontosturisticos;
+    
+    //entada de dados
+    printf("qual o código da carta ?\n");
+    scanf("%S",codigo);
+     
+    printf("qual o nome da carta ?\n");
+    scanf("%s",nome);
+
+    printf("qual o tamanho da população");
+    scanf("%d",populaçao);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ teste do chat.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    char codigo[10];
+    char nome[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+
+    // Entrada de dados
+    printf("Digite o código da cidade: ");
+    scanf("%s", codigo);
+
+    printf("Digite o nome da cidade: ");
+    scanf(" %[^\n]", nome); // permite espaços no nome
+
+    printf("Digite a população: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a área (em km²): ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pib);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &pontosTuristicos);
+
+    // Saída de dados
+    printf("\n--- Carta da Cidade ---\n");
+    printf("Código: %s\n", codigo);
+    printf("Nome: %s\n", nome);
+    printf("População: %d habitantes\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f bilhões\n", pib);
+    printf("Pontos turísticos: %d\n", pontosTuristicos);
+
+    return 0;
+}
