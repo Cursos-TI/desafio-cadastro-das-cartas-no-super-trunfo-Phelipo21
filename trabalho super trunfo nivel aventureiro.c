71a2d48c83e3ba19ec3700027461ca855a0e81b1
Phelipo21 <ypheliporego@gmail.com>
2025-09-11 15:30:13 +0000

    so a carta 2

--- trabalho super trunfo nivel aventureiro.c
+++ trabalho super trunfo nivel aventureiro.c
@@ -5,23 +5,20 @@ int main() {
     char codigo[50];
     char cidade [50];
     char estado [50];
-    float populaçao;
+    int populaçao;
     float area;
     float pib;
     int pontosturisticos;
     float densiadade;
     float pibpercapita;
 
-    densiadade = populaçao / area;
-     
-    pibpercapita = pib / populaçao;
 
     printf("\n#### super trunfo nível aventureiro ####\n");
-
+    printf("\n------   -------\n");
     //// entada de dados carta 1 /////
-    printf("\ncarta 1\n");
+   
 
-    printf("qual o codigo da Cidade ?: ");
+    printf(" qual o codigo da Cidade ?: ");
 
     scanf("%s",codigo);
 
@@ -29,19 +26,57 @@ int main() {
 
     scanf("%s", cidade);
 
-    printf(" qual o nome do Estado ??? : ");
+    printf("qual o nome do Estado ??? : ");
     
     scanf("%s", estado);
 
-    printf("qual o tamanho da população ??? :");
+    printf(" qual o tamanho da população ??? :");
 
     scanf("%f",&populaçao);
 
-    printf("qual a área da ciadade ??? :");
+    printf(" qual a área da ciadade ??? :");
 
-    scanf("%f",area);
+    scanf("%f",&area);
+
+    printf("qual o pib da cidade ??? :");
+
+    scanf("%f",&pib);
+
+    printf(" quantos pontos turístícos ??? :");
+
+    scanf("%d",&pontosturisticos);
+
+      densiadade = populaçao / area;
+     
+    pibpercapita = pib / populaçao;
+
 
     
+    //// saida de dados /////
+    printf("\n------- carta 01 ---------\n");
+
+    printf("codigo : %s\n",codigo);
+    
+    printf("cidade : %s\n",cidade);
+
+    printf("estado : %s\n", estado);
+
+    printf("população : %d milhões \n", populaçao);
+
+    printf("área : %.2f km² \n",area);
+
+    printf("pib : %.2f bilhões \n",pib);
+
+    printf("pontos turístícos : %d \n",pontosturisticos);
+
+    printf(" densidade : %.1f mil por km² \n",densiadade);
+
+    printf("pib per capita : %.2f mil \n",pibpercapita);
+
+
+
+
+
 
 
 
